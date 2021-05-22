a3f7d921b8d10569180427a294d52188393ed0c3
urosjebog <urosukivuk@gmail.com>
2021-05-22 21:35:39 +0200

    edwardlynxic

---
+++ index.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+
+const client = new Discord.Client();
+
+const embed = new Discord.MessageEmbed()
+
+client.once('ready', () => {
+	console.log('Void is Online!');
+});
+
+client.on('guildMemberAdd', guildMember =>{
+    let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'Verify');
+const embed = new Discord.MessageEmbed()
+.setColor('#FF8000')
+.setDescription(`Hey <@${guildMember.user.id}>, welcome to 𝐋𝐲𝐧𝐱'𝐬 𝐬𝐞𝐫𝐯𝐞𝐫`)
+    guildMember.roles.add(welcomeRole);
+    guildMember.guild.channels.cache.get('839108013440237568').send(embed);
+});
+
+ client.login(token);
